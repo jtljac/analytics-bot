76a028edd574ee8e3e66a00e73126ed543eeb113
Jtljac <jtljac2@gmail.com>
2024-11-20 01:53:47 +0000

    Fixed notifications, made actually work, made exit on failure

--- update-analytics.sh
+++ update-analytics.sh
@@ -2,7 +2,7 @@
 
 # Token for your bot
 TOKEN=
-# Channel to send notifications on
+# (Optional) Channel to send notifications on
 NOTIFICATION_CHANNEL_ID=
 # Guild to analyse messages of
 GUILD_ID=
@@ -17,24 +17,25 @@ LINK_INDEX=true
 
 
 function sendNotification()
- {
-     if [ ! -z "${VAR}" ]; then
-         curl \
-         -H "Authorization: Bot ${TOKEN}" \
-         -H "User-Agent: myBotThing (http://some.url, v0.1)" \
-         -H "Content-Type: application/json" \
-         -X POST \
-         -d "{\"content\":\"$1\"}" \
-         "https://discordapp.com/api/channels/${NOTIFICATION_CHANNEL_ID}/messages"
-     fi
-     echo "Sent Message: $1"
- }
+{
+    if [ ! -z $NOTIFICATION_CHANNEL_ID ]; then
+        curl \
+        -H "Authorization: Bot ${TOKEN}" \
+        -H "User-Agent: myBotThing (http://some.url, v0.1)" \
+        -H "Content-Type: application/json" \
+        -X POST \
+        -d "{\"content\":\"$1\"}" \
+        "https://discordapp.com/api/channels/${NOTIFICATION_CHANNEL_ID}/messages"
+    fi
 
- # Check required variables set
- if [ -z "${TOKEN}" ]; echo "You must set the bot token to use this" && exit
- if [ -z "${GUILD_ID}" ]; echo "You must set the guild ID to use this" && exit
- if [ -z "${TMP_DIR}" ]; echo "You must set the tmp directory to write the exported messages into" && exit
- if [ -z "${DST_FILE}" ]; echo "You must set the dest file to write the resulting analytics to" && exit
+    echo $1
+}
+
+# Check required variables set
+if [ -z $TOKEN ] ; then echo "You must set the bot token to use this" && exit; fi
+if [ -z $GUILD_ID ] ; then echo "You must set the guild ID to use this" && exit; fi
+if [ -z $TMP_DIR ] ; then echo "You must set the tmp directory to write the exported messages into" && exit; fi
+if [ -z $DST_DIR ] ; then echo "You must set the dest file to write the resulting analytics to" && exit; fi
 
 sendNotification "Starting Analytics Update"
 
@@ -44,6 +45,7 @@ discord-chat-exporter-plus-cli exportguild -t "${TOKEN}" --include-threads All  
 
 if [ $? -ne 0 ]; then
     sendNotification "Failed to extract messages from Guild"
+    exit
 fi
 
 DST_NAME="$(date +%F).html"
@@ -54,12 +56,13 @@ npx chat-analytics -p "discord" -i "${TMP_DIR}/*.json" -o "${DST_FILE}"
 
 if [ $? -ne 0 ]; then
     sendNotification "Failed to generate analytics"
+    exit
 fi
 
 if [ "$LINK_INDEX" = true ] ; then
     rm -f ${INDEX_FILE}
     ln -s ${DST_FILE} ${INDEX_FILE}
-    sendNotification "Analytics updated, ${ENDPOINT}/DST_NAME"
+    sendNotification "Analytics updated, ${ENDPOINT}/${DST_NAME}"
 else
     sendNotification "Analytics updated, ${ENDPOINT}"
 fi
